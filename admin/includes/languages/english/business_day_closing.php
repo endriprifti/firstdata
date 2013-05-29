8d317b08b3c6ff60475df49e36023a75dbb067e2
endri prifti <endri@msn.com>
2013-05-29 22:11:56 +0300

    Create business_day_closing.php

---
+++ admin/includes/languages/english/business_day_closing.php
@@ -0,0 +1,24 @@
+<?php
+/*
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+
+  First Data Payment Module for osCommerce 2.3
+  Developed by Endri Prifti, March 31, 2013
+*/
+
+define('HEADING_TITLE',  		'Business Day Closing');
+
+define('CLOSE_DATE',			'Date of Closure');
+define('RESULT',				'Result');
+define('RESULT_CODE',			'Result Code');
+define('COUNT_REVERSAL',		'Number of Transaction Reversals');
+define('COUNT_TRANSACTION',		'Number of Transaction');
+define('AMOUNT_REVERSAL',		'Total Amount of Transaction Reversals');
+define('AMOUNT_TRANSACTION',	'Total Amount of Transactions');
+
+?>
